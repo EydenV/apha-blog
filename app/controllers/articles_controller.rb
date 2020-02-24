b1b3777925180a13671ff31fc1f7961507325440
EydenV <evillanueva@nearsoft.com>
2020-02-24 10:51:51 -0600

    Add before action in articles controller to avoid redundancy

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:edit,:update,:destroy,:show]
+
   def index
     @articles = Article.all
   end
@@ -47,7 +49,10 @@ class ArticlesController < ApplicationController
   end
 
   private
-  def article_params
-    params.require(:article).permit(:title, :description)
-  end
+    def article_params
+      params.require(:article).permit(:title, :description)
+    end
+    def set_article
+      @article = Article.find(params[:id])
+    end
 end
